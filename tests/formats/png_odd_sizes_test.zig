7b124e4b66cdbec124ac1872a3636caf0a240f50
mlarouche <michael.larouche@gmail.com>
2020-12-01 18:56:02 -0500

    Read s04i3p01 and s04n3p01 properly!

--- tests/formats/png_odd_sizes_test.zig
+++ tests/formats/png_odd_sizes_test.zig
@@ -285,3 +285,109 @@ test "Read s03n3p01 data properly" {
         }
     }
 }
+
+test "Read s04i3p01 data properly" {
+    const file = try testOpenFile(zigimg_test_allocator, "tests/fixtures/png/s04i3p01.png");
+    defer file.close();
+
+    var stream_source = std.io.StreamSource{ .file = file };
+
+    var pngFile = png.PNG.init(zigimg_test_allocator);
+    defer pngFile.deinit();
+
+    var pixelsOpt: ?color.ColorStorage = null;
+    try pngFile.read(stream_source.inStream(), stream_source.seekableStream(), &pixelsOpt);
+
+    defer {
+        if (pixelsOpt) |pixels| {
+            pixels.deinit(zigimg_test_allocator);
+        }
+    }
+
+    expectEq(pngFile.header.width, 4);
+    expectEq(pngFile.header.height, 4);
+
+    testing.expect(pixelsOpt != null);
+
+    if (pixelsOpt) |pixels| {
+        testing.expect(pixels == PixelFormat.Bpp1);
+
+        expectEq(pixels.Bpp1.palette.len, 2);
+
+        const firstColor = pixels.Bpp1.palette[0].toIntegerColor8();
+        expectEq(firstColor.R, 255);
+        expectEq(firstColor.G, 0);
+        expectEq(firstColor.B, 119);
+
+        const secondColor = pixels.Bpp1.palette[1].toIntegerColor8();
+        expectEq(secondColor.R, 255);
+        expectEq(secondColor.G, 255);
+        expectEq(secondColor.B, 0);
+
+        expectEq(pixels.Bpp1.indices.len, 4 * 4);
+
+        const expected = [_]u8{
+            1, 1, 1, 1,
+            1, 0, 0, 1,
+            1, 0, 0, 1,
+            1, 1, 1, 1,
+        };
+        var index: usize = 0;
+        while (index < 4 * 4) : (index += 1) {
+            expectEq(pixels.Bpp1.indices[index], @intCast(u1, expected[index]));
+        }
+    }
+}
+
+test "Read s04n3p01 data properly" {
+    const file = try testOpenFile(zigimg_test_allocator, "tests/fixtures/png/s04n3p01.png");
+    defer file.close();
+
+    var stream_source = std.io.StreamSource{ .file = file };
+
+    var pngFile = png.PNG.init(zigimg_test_allocator);
+    defer pngFile.deinit();
+
+    var pixelsOpt: ?color.ColorStorage = null;
+    try pngFile.read(stream_source.inStream(), stream_source.seekableStream(), &pixelsOpt);
+
+    defer {
+        if (pixelsOpt) |pixels| {
+            pixels.deinit(zigimg_test_allocator);
+        }
+    }
+
+    expectEq(pngFile.header.width, 4);
+    expectEq(pngFile.header.height, 4);
+
+    testing.expect(pixelsOpt != null);
+
+    if (pixelsOpt) |pixels| {
+        testing.expect(pixels == PixelFormat.Bpp1);
+
+        expectEq(pixels.Bpp1.palette.len, 2);
+
+        const firstColor = pixels.Bpp1.palette[0].toIntegerColor8();
+        expectEq(firstColor.R, 255);
+        expectEq(firstColor.G, 0);
+        expectEq(firstColor.B, 119);
+
+        const secondColor = pixels.Bpp1.palette[1].toIntegerColor8();
+        expectEq(secondColor.R, 255);
+        expectEq(secondColor.G, 255);
+        expectEq(secondColor.B, 0);
+
+        expectEq(pixels.Bpp1.indices.len, 4 * 4);
+
+        const expected = [_]u8{
+            1, 1, 1, 1,
+            1, 0, 0, 1,
+            1, 0, 0, 1,
+            1, 1, 1, 1,
+        };
+        var index: usize = 0;
+        while (index < 4 * 4) : (index += 1) {
+            expectEq(pixels.Bpp1.indices[index], @intCast(u1, expected[index]));
+        }
+    }
+}
