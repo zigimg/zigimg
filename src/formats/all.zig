4997ce7c0b678aed0e413696212193a316dbcfc7
Michaël Larouche <larouche@kde.org>
2021-12-23 13:16:12 -0500

    Add support for Quite OK image format with importing https://github.com/MasterQ32/zig-qoi (#62)

--- src/formats/all.zig
+++ src/formats/all.zig
@@ -4,6 +4,7 @@ pub const PCX = @import("pcx.zig").PCX;
 pub const PGM = @import("netpbm.zig").PGM;
 pub const PNG = @import("png.zig").PNG;
 pub const PPM = @import("netpbm.zig").PPM;
+pub const QOI = @import("qoi.zig").QOI;
 pub const TGA = @import("tga.zig").TGA;
 
 pub const ImageEncoderOptions = union(enum) {
@@ -11,6 +12,7 @@ pub const ImageEncoderOptions = union(enum) {
     pbm: PBM.EncoderOptions,
     pgm: PGM.EncoderOptions,
     ppm: PPM.EncoderOptions,
+    qoi: QOI.EncoderOptions,
 
     const Self = @This();
 
--- zigimg.zig
+++ zigimg.zig
@@ -1,17 +1,18 @@
 pub const AllFormats = @import("src/formats/all.zig");
-pub const FormatInterface = @import("src/format_interface.zig").FormatInterface;
-pub const Image = image.Image;
-pub const ImageFormat = image.ImageFormat;
-pub const ImageSeekStream = image.ImageSeekStream;
-pub const OctTreeQuantizer = @import("src/octree_quantizer.zig").OctTreeQuantizer;
-pub const PixelFormat = @import("src/pixel_format.zig").PixelFormat;
 pub const bmp = @import("src/formats/bmp.zig");
 pub const color = @import("src/color.zig");
 pub const errors = @import("src/errors.zig");
+pub const FormatInterface = @import("src/format_interface.zig").FormatInterface;
 pub const image = @import("src/image.zig");
+pub const Image = image.Image;
+pub const ImageFormat = image.ImageFormat;
+pub const ImageSeekStream = image.ImageSeekStream;
 pub const netpbm = @import("src/formats/netpbm.zig");
+pub const OctTreeQuantizer = @import("src/octree_quantizer.zig").OctTreeQuantizer;
 pub const pcx = @import("src/formats/pcx.zig");
+pub const PixelFormat = @import("src/pixel_format.zig").PixelFormat;
 pub const png = @import("src/formats/png.zig");
+pub const qoi = @import("src/formats/qoi.zig");
 pub const tga = @import("src/formats/tga.zig");
 
 test {
