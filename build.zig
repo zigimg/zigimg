515e08b34768419cf08b838f0109437064da943b
mlarouche <michael.larouche@gmail.com>
2022-05-28 11:45:18 -0400

    * Rework launch.json to have Debug tests for Windows and UNIX
    * Rework build.zig to remove explicit path for the test executable that works on both Windows and UNIX

--- build.zig
+++ build.zig
@@ -3,19 +3,17 @@ const Builder = @import("std").build.Builder;
 pub fn build(b: *Builder) void {
     const buildMode = b.standardReleaseOptions();
 
-    const test_out_path = "zig-out/zigimgtest";
-
     const zigimg_build_test = b.addTestExe("zigimgtest", "zigimg.zig");
     zigimg_build_test.setBuildMode(buildMode);
-    zigimg_build_test.emit_bin = .{ .emit_to = test_out_path};
+    zigimg_build_test.install();
 
-    const zigimg_test = b.addTest("zigimg.zig");
-    zigimg_test.setBuildMode(buildMode);
-    zigimg_test.emit_bin = .{ .emit_to = test_out_path};
-
-    const test_build_step = b.step("tests", "build library tests to " ++ test_out_path);
-    test_build_step.dependOn(&zigimg_build_test.step);
+    const run_test_cmd = zigimg_build_test.run();
+    run_test_cmd.step.dependOn(b.getInstallStep());
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&zigimg_test.step);
+    test_step.dependOn(&run_test_cmd.step);
+
+    const build_only_test_step = b.step("test_build_only", "Build the tests but does not run it");
+    build_only_test_step.dependOn(&zigimg_build_test.step);
+    build_only_test_step.dependOn(b.getInstallStep());
 }
