ff46f7b706ec6b26d074e7654206de1c643919cb
mlarouche <michael.larouche@gmail.com>
2022-08-02 13:29:58 -0400

    Port old code to newest changes

--- src/formats/gif.zig
+++ src/formats/gif.zig
@@ -1,15 +1,12 @@
-const Allocator = std.mem.Allocator;
 const FormatInterface = @import("../format_interface.zig").FormatInterface;
-const ImageFormat = image.ImageFormat;
-const ImageReader = image.ImageReader;
-const ImageInfo = image.ImageInfo;
-const ImageSeekStream = image.ImageSeekStream;
 const PixelFormat = @import("../pixel_format.zig").PixelFormat;
 const color = @import("../color.zig");
-const errors = @import("../errors.zig");
-const image = @import("../image.zig");
+const Image = @import("../Image.zig");
 const std = @import("std");
 const utils = @import("../utils.zig");
+
+const ImageReadError = Image.ReadError;
+const ImageWriteError = Image.WriteError;
 
 pub const GIFHeader = packed struct {
     magic: [3]u8,
@@ -32,25 +29,23 @@ pub const GIF = struct {
 
     pub fn formatInterface() FormatInterface {
         return FormatInterface{
-            .format = @ptrCast(FormatInterface.FormatFn, format),
-            .formatDetect = @ptrCast(FormatInterface.FormatDetectFn, formatDetect),
-            .readForImage = @ptrCast(FormatInterface.ReadForImageFn, readForImage),
-            .writeForImage = @ptrCast(FormatInterface.WriteForImageFn, writeForImage),
+            .format = format,
+            .formatDetect = formatDetect,
+            .readImage = readImage,
+            .writeImage = writeImage,
         };
     }
 
-    pub fn format() ImageFormat {
-        return ImageFormat.Gif;
+    pub fn format() Image.Format {
+        return Image.Format.gif;
     }
 
-    pub fn formatDetect(reader: ImageReader, seek_stream: ImageSeekStream) !bool {
+    pub fn formatDetect(stream: *Image.Stream) !bool {
         const GIF87Header = "GIF87a";
         const GIF89Header = "GIF89a";
 
-        _ = seek_stream;
-
         var header_buffer: [6]u8 = undefined;
-        const read_bytes = try reader.read(header_buffer[0..]);
+        const read_bytes = try stream.read(header_buffer[0..]);
         if (read_bytes < 6) {
             return false;
         }
@@ -58,29 +53,31 @@ pub const GIF = struct {
         return std.mem.eql(u8, header_buffer[0..], GIF87Header) or std.mem.eql(u8, header_buffer[0..], GIF89Header);
     }
 
-    pub fn readForImage(allocator: *Allocator, reader: ImageReader, seek_stream: ImageSeekStream, pixels_opt: *?color.ColorStorage) !ImageInfo {
+    pub fn readImage(allocator: std.mem.Allocator, stream: *Image.Stream) ImageReadError!Image {
+        var result = Image.init(allocator);
+        errdefer result.deinit();
+
         var gif = Self{};
 
-        try gif.read(allocator, reader, seek_stream, pixels_opt);
+        try gif.read(allocator, stream, &result.pixels);
 
-        var image_info = ImageInfo{};
-        image_info.width = gif.header.width;
-        image_info.height = gif.header.height;
-        return image_info;
+        result.width = @intCast(usize, gif.header.width);
+        result.height = @intCast(usize, gif.header.height);
+        return result;
     }
 
-    pub fn writeForImage(allocator: *Allocator, write_stream: image.ImageWriterStream, seek_stream: ImageSeekStream, pixels: color.ColorStorage, save_info: image.ImageSaveInfo) !void {
+    pub fn writeImage(allocator: std.mem.Allocator, write_stream: *Image.Stream, pixels: color.PixelStorage, save_info: Image.SaveInfo) Image.Stream.WriteError!void {
         _ = allocator;
         _ = write_stream;
-        _ = seek_stream;
         _ = pixels;
         _ = save_info;
     }
 
-    pub fn read(self: *Self, allocator: *Allocator, reader: ImageReader, seek_stream: ImageSeekStream, pixels_opt: *?color.ColorStorage) !void {
+    pub fn read(self: *Self, allocator: std.mem.Allocator, stream: *Image.Stream, pixels_opt: *?color.PixelStorage) ImageReadError!void {
         _ = allocator;
-        _ = seek_stream;
         _ = pixels_opt;
+
+        const reader = stream.reader();
 
         self.header = try utils.readStructLittle(reader, GIFHeader);
 
@@ -90,11 +87,11 @@ pub const GIF = struct {
         std.log.debug("has_global_table={}, global_table_size={},", .{ has_global_table, global_table_size });
 
         if (!std.mem.eql(u8, self.header.magic[0..], "GIF")) {
-            return errors.ImageError.InvalidMagicHeader;
+            return ImageReadError.InvalidData;
         }
 
         if (!(std.mem.eql(u8, self.header.version[0..], "87a") or std.mem.eql(u8, self.header.version[0..], "89a"))) {
-            return errors.ImageError.InvalidMagicHeader;
+            return ImageReadError.InvalidData;
         }
     }
 };
