86acbbe0bcad0df033d16c5eb3488431df488f21
LeRoyce Pearson <contact@leroycepearson.dev>
2023-04-19 01:50:00 -0600

    Use std.Build.installArtifact

--- build.zig
+++ build.zig
@@ -14,12 +14,11 @@ pub fn build(b: *Build) void {
         .target = target,
         .optimize = optimize,
     });
-    zigimg_build_test.install();
+    b.installArtifact(zigimg_build_test);
 
-    const run_test_cmd = zigimg_build_test.run();
+    const run_test_cmd = b.addRunArtifact(zigimg_build_test);
     // Force running of the test command even if you don't have changes
     run_test_cmd.has_side_effects = true;
-    run_test_cmd.step.dependOn(b.getInstallStep());
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_test_cmd.step);
