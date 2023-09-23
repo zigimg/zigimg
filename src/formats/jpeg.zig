aa9dd2df31f6b9e0f4885365b9090fadbeb90494
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-09-23 22:35:35 +0200

    refactor: move huffman table to own file

--- src/formats/jpeg.zig
+++ src/formats/jpeg.zig
@@ -18,6 +18,10 @@ const ScanComponentSpec = ScanHeader.ScanComponentSpec;
 const ZigzagOffsets = @import("./jpeg/utils.zig").ZigzagOffsets;
 const IDCTMultipliers = @import("./jpeg/utils.zig").IDCTMultipliers;
 const QuantizationTable = @import("./jpeg/quantization.zig").Table;
+
+const HuffmanReader = @import("./jpeg/huffman.zig").Reader;
+const HuffmanTable =  @import("./jpeg/huffman.zig").Table;
+
 // TODO: Chroma subsampling
 // TODO: Progressive scans
 // TODO: Non-baseline sequential DCT
@@ -30,164 +34,6 @@ const JPEG_VERY_DEBUG = false;
 const MAX_COMPONENTS = 3;
 const MAX_BLOCKS = 8;
 const MCU = [64]i32;
-
-const HuffmanCode = struct { length_minus_one: u4, code: u16 };
-const HuffmanCodeMap = std.AutoArrayHashMap(HuffmanCode, u8);
-
-const HuffmanTable = struct {
-    allocator: Allocator,
-
-    code_counts: [16]u8,
-    code_map: HuffmanCodeMap,
-
-    table_class: u8,
-
-    pub fn read(allocator: Allocator, table_class: u8, reader: Image.Stream.Reader) ImageReadError!HuffmanTable {
-        if (table_class & 1 != table_class)
-            return ImageReadError.InvalidData;
-
-        var code_counts: [16]u8 = undefined;
-        if ((try reader.read(code_counts[0..])) < 16) {
-            return ImageReadError.InvalidData;
-        }
-
-        if (JPEG_DEBUG) std.debug.print("  Code counts: {any}\n", .{code_counts});
-
-        var total_huffman_codes: usize = 0;
-        for (code_counts) |count| total_huffman_codes += count;
-
-        var huffman_code_map = HuffmanCodeMap.init(allocator);
-        errdefer huffman_code_map.deinit();
-
-        if (JPEG_VERY_DEBUG) std.debug.print("  Decoded huffman codes map:\n", .{});
-
-        var code: u16 = 0;
-        for (code_counts, 0..) |count, i| {
-            if (JPEG_VERY_DEBUG) {
-                std.debug.print("    Length {}: ", .{i + 1});
-                if (count == 0) {
-                    std.debug.print("(none)\n", .{});
-                } else {
-                    std.debug.print("\n", .{});
-                }
-            }
-
-            var j: usize = 0;
-            while (j < count) : (j += 1) {
-                // Check if we hit all 1s, i.e. 111111 for i == 6, which is an invalid value
-                if (code == (@as(u17, @intCast(1)) << (@as(u5, @intCast(i)) + 1)) - 1) {
-                    return ImageReadError.InvalidData;
-                }
-
-                const byte = try reader.readByte();
-                try huffman_code_map.put(.{ .length_minus_one = @as(u4, @intCast(i)), .code = code }, byte);
-
-                if (JPEG_VERY_DEBUG) std.debug.print("      {b} => 0x{X}\n", .{ code, byte });
-                code += 1;
-            }
-
-            code <<= 1;
-        }
-
-        return HuffmanTable{
-            .allocator = allocator,
-            .code_counts = code_counts,
-            .code_map = huffman_code_map,
-            .table_class = table_class,
-        };
-    }
-
-    pub fn deinit(self: *HuffmanTable) void {
-        self.code_map.deinit();
-    }
-};
-
-const HuffmanReader = struct {
-    table: ?*const HuffmanTable = null,
-    reader: Image.Stream.Reader,
-    byte_buffer: u8 = 0,
-    bits_left: u4 = 0,
-    last_byte_was_ff: bool = false,
-
-    pub fn init(reader: Image.Stream.Reader) HuffmanReader {
-        return .{
-            .reader = reader,
-        };
-    }
-
-    pub fn setHuffmanTable(self: *HuffmanReader, table: *const HuffmanTable) void {
-        self.table = table;
-    }
-
-    fn readBit(self: *HuffmanReader) ImageReadError!u1 {
-        if (self.bits_left == 0) {
-            self.byte_buffer = try self.reader.readByte();
-
-            if (self.byte_buffer == 0 and self.last_byte_was_ff) {
-                // This was a stuffed byte, read one more.
-                self.byte_buffer = try self.reader.readByte();
-            }
-            self.last_byte_was_ff = self.byte_buffer == 0xFF;
-            self.bits_left = 8;
-        }
-
-        const bit: u1 = @intCast(self.byte_buffer >> 7);
-        self.byte_buffer <<= 1;
-        self.bits_left -= 1;
-
-        return bit;
-    }
-
-    pub fn readCode(self: *HuffmanReader) ImageReadError!u8 {
-        var code: u16 = 0;
-
-        var i: u5 = 0;
-        while (i < 16) : (i += 1) {
-            code = (code << 1) | (try self.readBit());
-            if (self.table.?.code_map.get(.{ .length_minus_one = @intCast(i), .code = code })) |value| {
-                return value;
-            }
-        }
-
-        if (JPEG_DEBUG) std.debug.print("found unknown code: {x}\n", .{code});
-        return ImageReadError.InvalidData;
-    }
-
-    pub fn readLiteralBits(self: *HuffmanReader, bitsNeeded: u8) ImageReadError!u32 {
-        var bits: u32 = 0;
-
-        var i: usize = 0;
-        while (i < bitsNeeded) : (i += 1) {
-            bits = (bits << 1) | (try self.readBit());
-        }
-
-        return bits;
-    }
-
-    /// This function implements T.81 section F1.2.1, Huffman encoding of DC coefficients.
-    pub fn readMagnitudeCoded(self: *HuffmanReader, magnitude: u5) ImageReadError!i32 {
-        if (magnitude == 0)
-            return 0;
-
-        const bits = try self.readLiteralBits(magnitude);
-
-        // The sign of the read bits value.
-        const bits_sign = (bits >> (magnitude - 1)) & 1;
-        // The mask for clearing the sign bit.
-        const bits_mask = (@as(u32, 1) << (magnitude - 1)) - 1;
-        // The bits without the sign bit.
-        const unsigned_bits = bits & bits_mask;
-
-        // The magnitude base value. This is -2^n+1 when bits_sign == 0, and
-        // 2^(n-1) when bits_sign == 1.
-        const base = if (bits_sign == 0)
-            -(@as(i32, 1) << magnitude) + 1
-        else
-            (@as(i32, 1) << (magnitude - 1));
-
-        return base + @as(i32, @bitCast(unsigned_bits));
-    }
-};
 
 const Scan = struct {
     pub fn performScan(frame: *const Frame, reader: Image.Stream.Reader, pixels_opt: *?color.PixelStorage) ImageReadError!void {
