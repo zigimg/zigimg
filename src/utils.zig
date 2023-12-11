4a3ec0b2aa7861a5d6829d58f24a3071d88063ad
mlarouche <michael.larouche@gmail.com>
2023-12-11 09:21:49 -0500

    run zig fmt

--- src/utils.zig
+++ src/utils.zig
@@ -36,7 +36,7 @@ pub fn toMagicNumberForeign(magic: []const u8) u32 {
 }
 
 pub inline fn toMagicNumber(magic: []const u8, comptime wanted_endian: std.builtin.Endian) u32 {
-      return switch (native_endian) {
+    return switch (native_endian) {
         .little => {
             return switch (wanted_endian) {
                 .little => toMagicNumberNative(magic),
