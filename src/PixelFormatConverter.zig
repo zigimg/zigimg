e647a98a2cb07f2a15697d6c7d13a42bb8b81e2a
mlarouche <michael.larouche@gmail.com>
2024-05-26 17:13:26 -0400

    * Add remaining indexed to RGB/BGR, RGBA/BGRA: RGB555, RGB565, RGB24, BGR555, BGR24,BGRA32, RGB48, RGBA64

--- src/PixelFormatConverter.zig
+++ src/PixelFormatConverter.zig
@@ -27,12 +27,68 @@ pub fn convert(allocator: std.mem.Allocator, source: *const color.PixelStorage, 
 
         conversionId(.indexed8, .indexed16) => IndexedSmallToLarge(.indexed8, .indexed16).convert(source, &destination),
 
+        // Indexed -> RGB555
+        conversionId(.indexed1, .rgb555) => IndexedToRgbColor(.indexed1, .rgb555).convert(source, &destination),
+        conversionId(.indexed2, .rgb555) => IndexedToRgbColor(.indexed2, .rgb555).convert(source, &destination),
+        conversionId(.indexed4, .rgb555) => IndexedToRgbColor(.indexed4, .rgb555).convert(source, &destination),
+        conversionId(.indexed8, .rgb555) => IndexedToRgbColor(.indexed8, .rgb555).convert(source, &destination),
+        conversionId(.indexed16, .rgb555) => IndexedToRgbColor(.indexed16, .rgb555).convert(source, &destination),
+
+        // Indexed -> RGB565
+        conversionId(.indexed1, .rgb565) => IndexedToRgbColor(.indexed1, .rgb565).convert(source, &destination),
+        conversionId(.indexed2, .rgb565) => IndexedToRgbColor(.indexed2, .rgb565).convert(source, &destination),
+        conversionId(.indexed4, .rgb565) => IndexedToRgbColor(.indexed4, .rgb565).convert(source, &destination),
+        conversionId(.indexed8, .rgb565) => IndexedToRgbColor(.indexed8, .rgb565).convert(source, &destination),
+        conversionId(.indexed16, .rgb565) => IndexedToRgbColor(.indexed16, .rgb565).convert(source, &destination),
+
+        // Indexed -> RGB24
+        conversionId(.indexed1, .rgb24) => IndexedToRgbColor(.indexed1, .rgb24).convert(source, &destination),
+        conversionId(.indexed2, .rgb24) => IndexedToRgbColor(.indexed2, .rgb24).convert(source, &destination),
+        conversionId(.indexed4, .rgb24) => IndexedToRgbColor(.indexed4, .rgb24).convert(source, &destination),
+        conversionId(.indexed8, .rgb24) => IndexedToRgbColor(.indexed8, .rgb24).convert(source, &destination),
+        conversionId(.indexed16, .rgb24) => IndexedToRgbColor(.indexed16, .rgb24).convert(source, &destination),
+
         // Indexed -> RGBA32
         conversionId(.indexed1, .rgba32) => indexedToRgba32(.indexed1, source, &destination),
         conversionId(.indexed2, .rgba32) => indexedToRgba32(.indexed2, source, &destination),
         conversionId(.indexed4, .rgba32) => indexedToRgba32(.indexed4, source, &destination),
         conversionId(.indexed8, .rgba32) => indexedToRgba32(.indexed8, source, &destination),
         conversionId(.indexed16, .rgba32) => indexedToRgba32(.indexed16, source, &destination),
+
+        // Indexed -> BGR555
+        conversionId(.indexed1, .bgr555) => IndexedToRgbColor(.indexed1, .bgr555).convert(source, &destination),
+        conversionId(.indexed2, .bgr555) => IndexedToRgbColor(.indexed2, .bgr555).convert(source, &destination),
+        conversionId(.indexed4, .bgr555) => IndexedToRgbColor(.indexed4, .bgr555).convert(source, &destination),
+        conversionId(.indexed8, .bgr555) => IndexedToRgbColor(.indexed8, .bgr555).convert(source, &destination),
+        conversionId(.indexed16, .bgr555) => IndexedToRgbColor(.indexed16, .bgr555).convert(source, &destination),
+
+        // Indexed -> BGR24
+        conversionId(.indexed1, .bgr24) => IndexedToRgbColor(.indexed1, .bgr24).convert(source, &destination),
+        conversionId(.indexed2, .bgr24) => IndexedToRgbColor(.indexed2, .bgr24).convert(source, &destination),
+        conversionId(.indexed4, .bgr24) => IndexedToRgbColor(.indexed4, .bgr24).convert(source, &destination),
+        conversionId(.indexed8, .bgr24) => IndexedToRgbColor(.indexed8, .bgr24).convert(source, &destination),
+        conversionId(.indexed16, .bgr24) => IndexedToRgbColor(.indexed16, .bgr24).convert(source, &destination),
+
+        // Indexed -> BGRA32
+        conversionId(.indexed1, .bgra32) => IndexedToRgbaColor(.indexed1, .bgra32).convert(source, &destination),
+        conversionId(.indexed2, .bgra32) => IndexedToRgbaColor(.indexed2, .bgra32).convert(source, &destination),
+        conversionId(.indexed4, .bgra32) => IndexedToRgbaColor(.indexed4, .bgra32).convert(source, &destination),
+        conversionId(.indexed8, .bgra32) => IndexedToRgbaColor(.indexed8, .bgra32).convert(source, &destination),
+        conversionId(.indexed16, .bgra32) => IndexedToRgbaColor(.indexed16, .bgra32).convert(source, &destination),
+
+        // Indexed -> RGB48
+        conversionId(.indexed1, .rgb48) => IndexedToRgbColor(.indexed1, .rgb48).convert(source, &destination),
+        conversionId(.indexed2, .rgb48) => IndexedToRgbColor(.indexed2, .rgb48).convert(source, &destination),
+        conversionId(.indexed4, .rgb48) => IndexedToRgbColor(.indexed4, .rgb48).convert(source, &destination),
+        conversionId(.indexed8, .rgb48) => IndexedToRgbColor(.indexed8, .rgb48).convert(source, &destination),
+        conversionId(.indexed16, .rgb48) => IndexedToRgbColor(.indexed16, .rgb48).convert(source, &destination),
+
+        // Indexed -> RGBA64
+        conversionId(.indexed1, .rgba64) => IndexedToRgbaColor(.indexed1, .rgba64).convert(source, &destination),
+        conversionId(.indexed2, .rgba64) => IndexedToRgbaColor(.indexed2, .rgba64).convert(source, &destination),
+        conversionId(.indexed4, .rgba64) => IndexedToRgbaColor(.indexed4, .rgba64).convert(source, &destination),
+        conversionId(.indexed8, .rgba64) => IndexedToRgbaColor(.indexed8, .rgba64).convert(source, &destination),
+        conversionId(.indexed16, .rgba64) => IndexedToRgbaColor(.indexed16, .rgba64).convert(source, &destination),
 
         // Indexed -> Colorf32
         conversionId(.indexed1, .float32) => indexedToColorf32(.indexed1, source, &destination),
@@ -73,6 +129,70 @@ fn IndexedSmallToLarge(comptime source_format: PixelFormat, comptime destination
 
             for (0..source_indexed.indices.len) |index| {
                 destination_indexed.indices[index] = source_indexed.indices[index];
+            }
+        }
+    };
+}
+
+fn rgbToRgb(comptime T: type, rgb: anytype) T {
+    return T{
+        .r = color.scaleToIntColor(std.meta.fieldInfo(T, .r).type, rgb.r),
+        .g = color.scaleToIntColor(std.meta.fieldInfo(T, .g).type, rgb.g),
+        .b = color.scaleToIntColor(std.meta.fieldInfo(T, .b).type, rgb.b),
+    };
+}
+
+fn rgbToRgba(comptime T: type, rgb: anytype) T {
+    return T{
+        .r = color.scaleToIntColor(std.meta.fieldInfo(T, .r).type, rgb.r),
+        .g = color.scaleToIntColor(std.meta.fieldInfo(T, .g).type, rgb.g),
+        .b = color.scaleToIntColor(std.meta.fieldInfo(T, .b).type, rgb.b),
+        .a = 255,
+    };
+}
+
+fn rgbaToRgb(comptime T: type, rgba: anytype) T {
+    const alpha = color.toF32Color(rgba.a);
+
+    return T{
+        .r = color.toIntColor(std.meta.fieldInfo(T, .r).type, color.toF32Color(rgba.r) * alpha),
+        .g = color.toIntColor(std.meta.fieldInfo(T, .g).type, color.toF32Color(rgba.g) * alpha),
+        .b = color.toIntColor(std.meta.fieldInfo(T, .b).type, color.toF32Color(rgba.b) * alpha),
+    };
+}
+
+fn rgbaToRgba(comptime T: type, rgba: anytype) T {
+    return T{
+        .r = color.scaleToIntColor(std.meta.fieldInfo(T, .r).type, rgba.r),
+        .g = color.scaleToIntColor(std.meta.fieldInfo(T, .g).type, rgba.g),
+        .b = color.scaleToIntColor(std.meta.fieldInfo(T, .b).type, rgba.b),
+        .a = color.scaleToIntColor(std.meta.fieldInfo(T, .a).type, rgba.a),
+    };
+}
+
+fn IndexedToRgbColor(comptime source_format: PixelFormat, comptime destination_format: PixelFormat) type {
+    return struct {
+        pub fn convert(source: *const color.PixelStorage, destination: *color.PixelStorage) void {
+            const source_indexed = @field(source, getFieldNameFromPixelFormat(source_format));
+            var destination_pixels = @field(destination, getFieldNameFromPixelFormat(destination_format));
+            const destination_type = @TypeOf(destination_pixels[0]);
+
+            for (0..source_indexed.indices.len) |index| {
+                destination_pixels[index] = rgbaToRgb(destination_type, source_indexed.palette[source_indexed.indices[index]]);
+            }
+        }
+    };
+}
+
+fn IndexedToRgbaColor(comptime source_format: PixelFormat, comptime destination_format: PixelFormat) type {
+    return struct {
+        pub fn convert(source: *const color.PixelStorage, destination: *color.PixelStorage) void {
+            const source_indexed = @field(source, getFieldNameFromPixelFormat(source_format));
+            var destination_pixels = @field(destination, getFieldNameFromPixelFormat(destination_format));
+            const destination_type = @TypeOf(destination_pixels[0]);
+
+            for (0..source_indexed.indices.len) |index| {
+                destination_pixels[index] = rgbaToRgba(destination_type, source_indexed.palette[source_indexed.indices[index]]);
             }
         }
     };
--- tests/pixel_format_converter_test.zig
+++ tests/pixel_format_converter_test.zig
@@ -4,20 +4,38 @@ const color = @import("../src/color.zig");
 const PixelFormatConverter = @import("../src/PixelFormatConverter.zig");
 const helpers = @import("helpers.zig");
 
-const red_rgba32 = color.Rgba32.initRgb(255, 0, 0);
-const green_rgba32 = color.Rgba32.initRgb(0, 255, 0);
-const blue_rgba32 = color.Rgba32.initRgb(0, 0, 255);
-
 const red_float32 = color.Colorf32.initRgb(1.0, 0.0, 0.0);
 const green_float32 = color.Colorf32.initRgb(0.0, 1.0, 0.0);
 const blue_float32 = color.Colorf32.initRgb(0.0, 0.0, 1.0);
+
+fn Colors(comptime T: type) type {
+    return struct {
+        const RedT = std.meta.fieldInfo(T, .r).type;
+        const GreenT = std.meta.fieldInfo(T, .g).type;
+        const BlueT = std.meta.fieldInfo(T, .b).type;
+
+        pub const red = T.initRgb(std.math.maxInt(RedT), 0, 0);
+        pub const green = T.initRgb(0, std.math.maxInt(GreenT), 0);
+        pub const blue = T.initRgb(0, 0, std.math.maxInt(BlueT));
+        pub const white = T.initRgb(std.math.maxInt(RedT), std.math.maxInt(GreenT), std.math.maxInt(BlueT));
+        pub const black = T.initRgb(0, 0, 0);
+    };
+}
+
+const Colorsf32 = struct {
+    pub const red = color.Colorf32.initRgb(1.0, 0.0, 0.0);
+    pub const green = color.Colorf32.initRgb(0.0, 1.0, 0.0);
+    pub const blue = color.Colorf32.initRgb(0.0, 0.0, 1.0);
+    pub const white = color.Colorf32.initRgb(1.0, 1.0, 1.0);
+    pub const black = color.Colorf32.initRgb(0.0, 0.0, 0.0);
+};
 
 test "PixelFormatConverter: convert from indexed1 to indexed2" {
     const indexed1_pixels = try color.PixelStorage.init(helpers.zigimg_test_allocator, .indexed1, 4);
     defer indexed1_pixels.deinit(helpers.zigimg_test_allocator);
 
-    indexed1_pixels.indexed1.palette[0] = red_rgba32;
-    indexed1_pixels.indexed1.palette[1] = green_rgba32;
+    indexed1_pixels.indexed1.palette[0] = Colors(color.Rgba32).red;
+    indexed1_pixels.indexed1.palette[1] = Colors(color.Rgba32).green;
     indexed1_pixels.indexed1.indices[0] = 0;
     indexed1_pixels.indexed1.indices[1] = 1;
     indexed1_pixels.indexed1.indices[2] = 1;
@@ -39,8 +57,8 @@ test "PixelFormatConverter: convert from indexed1 to indexed16" {
     const indexed1_pixels = try color.PixelStorage.init(helpers.zigimg_test_allocator, .indexed1, 4);
     defer indexed1_pixels.deinit(helpers.zigimg_test_allocator);
 
-    indexed1_pixels.indexed1.palette[0] = red_rgba32;
-    indexed1_pixels.indexed1.palette[1] = green_rgba32;
+    indexed1_pixels.indexed1.palette[0] = Colors(color.Rgba32).red;
+    indexed1_pixels.indexed1.palette[1] = Colors(color.Rgba32).green;
     indexed1_pixels.indexed1.indices[0] = 0;
     indexed1_pixels.indexed1.indices[1] = 1;
     indexed1_pixels.indexed1.indices[2] = 1;
@@ -58,42 +76,232 @@ test "PixelFormatConverter: convert from indexed1 to indexed16" {
     try helpers.expectEq(indexed16_pixels.indexed16.indices[3], 0);
 }
 
-test "PixelFormatConverter: convert from indexed1 to rgba32" {
-    const indexed1_pixels = try color.PixelStorage.init(helpers.zigimg_test_allocator, .indexed1, 4);
-    defer indexed1_pixels.deinit(helpers.zigimg_test_allocator);
+test "PixelFormatConverter: convert from indexed2 to rgb555" {
+    const indexed2_pixels = try color.PixelStorage.init(helpers.zigimg_test_allocator, .indexed2, 4);
+    defer indexed2_pixels.deinit(helpers.zigimg_test_allocator);
 
-    indexed1_pixels.indexed1.palette[0] = red_rgba32;
-    indexed1_pixels.indexed1.palette[1] = green_rgba32;
-    indexed1_pixels.indexed1.indices[0] = 0;
-    indexed1_pixels.indexed1.indices[1] = 1;
-    indexed1_pixels.indexed1.indices[2] = 1;
-    indexed1_pixels.indexed1.indices[3] = 0;
+    indexed2_pixels.indexed2.palette[0] = Colors(color.Rgba32).red;
+    indexed2_pixels.indexed2.palette[1] = Colors(color.Rgba32).green;
+    indexed2_pixels.indexed2.palette[2] = Colors(color.Rgba32).blue;
+    indexed2_pixels.indexed2.palette[3] = Colors(color.Rgba32).white;
 
-    const rgba32_pixels = try PixelFormatConverter.convert(helpers.zigimg_test_allocator, &indexed1_pixels, .rgba32);
-    defer rgba32_pixels.deinit(helpers.zigimg_test_allocator);
+    indexed2_pixels.indexed2.indices[0] = 0;
+    indexed2_pixels.indexed2.indices[1] = 1;
+    indexed2_pixels.indexed2.indices[2] = 2;
+    indexed2_pixels.indexed2.indices[3] = 3;
 
-    try helpers.expectEq(rgba32_pixels.rgba32[0], red_rgba32);
-    try helpers.expectEq(rgba32_pixels.rgba32[1], green_rgba32);
-    try helpers.expectEq(rgba32_pixels.rgba32[2], green_rgba32);
-    try helpers.expectEq(rgba32_pixels.rgba32[3], red_rgba32);
+    const rgb555_pixels = try PixelFormatConverter.convert(helpers.zigimg_test_allocator, &indexed2_pixels, .rgb555);
+    defer rgb555_pixels.deinit(helpers.zigimg_test_allocator);
+
+    try helpers.expectEq(rgb555_pixels.rgb555[0], Colors(color.Rgb555).red);
+    try helpers.expectEq(rgb555_pixels.rgb555[1], Colors(color.Rgb555).green);
+    try helpers.expectEq(rgb555_pixels.rgb555[2], Colors(color.Rgb555).blue);
+    try helpers.expectEq(rgb555_pixels.rgb555[3], Colors(color.Rgb555).white);
 }
 
-test "PixelFormatConverter: convert from indexed1 to Colorf32" {
-    const indexed1_pixels = try color.PixelStorage.init(helpers.zigimg_test_allocator, .indexed1, 4);
-    defer indexed1_pixels.deinit(helpers.zigimg_test_allocator);
+test "PixelFormatConverter: convert from indexed2 to rgb565" {
+    const indexed2_pixels = try color.PixelStorage.init(helpers.zigimg_test_allocator, .indexed2, 4);
+    defer indexed2_pixels.deinit(helpers.zigimg_test_allocator);
 
-    indexed1_pixels.indexed1.palette[0] = red_rgba32;
-    indexed1_pixels.indexed1.palette[1] = green_rgba32;
-    indexed1_pixels.indexed1.indices[0] = 0;
-    indexed1_pixels.indexed1.indices[1] = 1;
-    indexed1_pixels.indexed1.indices[2] = 1;
-    indexed1_pixels.indexed1.indices[3] = 0;
+    indexed2_pixels.indexed2.palette[0] = Colors(color.Rgba32).red;
+    indexed2_pixels.indexed2.palette[1] = Colors(color.Rgba32).green;
+    indexed2_pixels.indexed2.palette[2] = Colors(color.Rgba32).blue;
+    indexed2_pixels.indexed2.palette[3] = Colors(color.Rgba32).white;
 
-    const float32_pixels = try PixelFormatConverter.convert(helpers.zigimg_test_allocator, &indexed1_pixels, .float32);
+    indexed2_pixels.indexed2.indices[0] = 0;
+    indexed2_pixels.indexed2.indices[1] = 1;
+    indexed2_pixels.indexed2.indices[2] = 2;
+    indexed2_pixels.indexed2.indices[3] = 3;
+
+    const rgb565_pixels = try PixelFormatConverter.convert(helpers.zigimg_test_allocator, &indexed2_pixels, .rgb565);
+    defer rgb565_pixels.deinit(helpers.zigimg_test_allocator);
+
+    try helpers.expectEq(rgb565_pixels.rgb565[0], Colors(color.Rgb565).red);
+    try helpers.expectEq(rgb565_pixels.rgb565[1], Colors(color.Rgb565).green);
+    try helpers.expectEq(rgb565_pixels.rgb565[2], Colors(color.Rgb565).blue);
+    try helpers.expectEq(rgb565_pixels.rgb565[3], Colors(color.Rgb565).white);
+}
+
+test "PixelFormatConverter: convert from indexed2 to rgb24" {
+    const indexed2_pixels = try color.PixelStorage.init(helpers.zigimg_test_allocator, .indexed2, 4);
+    defer indexed2_pixels.deinit(helpers.zigimg_test_allocator);
+
+    indexed2_pixels.indexed2.palette[0] = Colors(color.Rgba32).red;
+    indexed2_pixels.indexed2.palette[1] = Colors(color.Rgba32).green;
+    indexed2_pixels.indexed2.palette[2] = Colors(color.Rgba32).blue;
+    indexed2_pixels.indexed2.palette[3] = Colors(color.Rgba32).white;
+
+    indexed2_pixels.indexed2.indices[0] = 0;
+    indexed2_pixels.indexed2.indices[1] = 1;
+    indexed2_pixels.indexed2.indices[2] = 2;
+    indexed2_pixels.indexed2.indices[3] = 3;
+
+    const rgb24_pixels = try PixelFormatConverter.convert(helpers.zigimg_test_allocator, &indexed2_pixels, .rgb24);
+    defer rgb24_pixels.deinit(helpers.zigimg_test_allocator);
+
+    try helpers.expectEq(rgb24_pixels.rgb24[0], Colors(color.Rgb24).red);
+    try helpers.expectEq(rgb24_pixels.rgb24[1], Colors(color.Rgb24).green);
+    try helpers.expectEq(rgb24_pixels.rgb24[2], Colors(color.Rgb24).blue);
+    try helpers.expectEq(rgb24_pixels.rgb24[3], Colors(color.Rgb24).white);
+}
+
+test "PixelFormatConverter: convert from indexed2 to rgba32" {
+    const indexed2_pixels = try color.PixelStorage.init(helpers.zigimg_test_allocator, .indexed2, 4);
+    defer indexed2_pixels.deinit(helpers.zigimg_test_allocator);
+
+    indexed2_pixels.indexed2.palette[0] = Colors(color.Rgba32).red;
+    indexed2_pixels.indexed2.palette[1] = Colors(color.Rgba32).green;
+    indexed2_pixels.indexed2.palette[2] = Colors(color.Rgba32).blue;
+    indexed2_pixels.indexed2.palette[3] = Colors(color.Rgba32).white;
+
+    indexed2_pixels.indexed2.indices[0] = 0;
+    indexed2_pixels.indexed2.indices[1] = 1;
+    indexed2_pixels.indexed2.indices[2] = 2;
+    indexed2_pixels.indexed2.indices[3] = 3;
+
+    const rgba32_pixels = try PixelFormatConverter.convert(helpers.zigimg_test_allocator, &indexed2_pixels, .rgba32);
+    defer rgba32_pixels.deinit(helpers.zigimg_test_allocator);
+
+    try helpers.expectEq(rgba32_pixels.rgba32[0], Colors(color.Rgba32).red);
+    try helpers.expectEq(rgba32_pixels.rgba32[1], Colors(color.Rgba32).green);
+    try helpers.expectEq(rgba32_pixels.rgba32[2], Colors(color.Rgba32).blue);
+    try helpers.expectEq(rgba32_pixels.rgba32[3], Colors(color.Rgba32).white);
+}
+
+test "PixelFormatConverter: convert from indexed2 to bgr555" {
+    const indexed2_pixels = try color.PixelStorage.init(helpers.zigimg_test_allocator, .indexed2, 4);
+    defer indexed2_pixels.deinit(helpers.zigimg_test_allocator);
+
+    indexed2_pixels.indexed2.palette[0] = Colors(color.Rgba32).red;
+    indexed2_pixels.indexed2.palette[1] = Colors(color.Rgba32).green;
+    indexed2_pixels.indexed2.palette[2] = Colors(color.Rgba32).blue;
+    indexed2_pixels.indexed2.palette[3] = Colors(color.Rgba32).white;
+
+    indexed2_pixels.indexed2.indices[0] = 0;
+    indexed2_pixels.indexed2.indices[1] = 1;
+    indexed2_pixels.indexed2.indices[2] = 2;
+    indexed2_pixels.indexed2.indices[3] = 3;
+
+    const bgr555_pixels = try PixelFormatConverter.convert(helpers.zigimg_test_allocator, &indexed2_pixels, .bgr555);
+    defer bgr555_pixels.deinit(helpers.zigimg_test_allocator);
+
+    try helpers.expectEq(bgr555_pixels.bgr555[0], Colors(color.Bgr555).red);
+    try helpers.expectEq(bgr555_pixels.bgr555[1], Colors(color.Bgr555).green);
+    try helpers.expectEq(bgr555_pixels.bgr555[2], Colors(color.Bgr555).blue);
+    try helpers.expectEq(bgr555_pixels.bgr555[3], Colors(color.Bgr555).white);
+}
+
+test "PixelFormatConverter: convert from indexed2 to bgr24" {
+    const indexed2_pixels = try color.PixelStorage.init(helpers.zigimg_test_allocator, .indexed2, 4);
+    defer indexed2_pixels.deinit(helpers.zigimg_test_allocator);
+
+    indexed2_pixels.indexed2.palette[0] = Colors(color.Rgba32).red;
+    indexed2_pixels.indexed2.palette[1] = Colors(color.Rgba32).green;
+    indexed2_pixels.indexed2.palette[2] = Colors(color.Rgba32).blue;
+    indexed2_pixels.indexed2.palette[3] = Colors(color.Rgba32).white;
+
+    indexed2_pixels.indexed2.indices[0] = 0;
+    indexed2_pixels.indexed2.indices[1] = 1;
+    indexed2_pixels.indexed2.indices[2] = 2;
+    indexed2_pixels.indexed2.indices[3] = 3;
+
+    const bgr24_pixels = try PixelFormatConverter.convert(helpers.zigimg_test_allocator, &indexed2_pixels, .bgr24);
+    defer bgr24_pixels.deinit(helpers.zigimg_test_allocator);
+
+    try helpers.expectEq(bgr24_pixels.bgr24[0], Colors(color.Bgr24).red);
+    try helpers.expectEq(bgr24_pixels.bgr24[1], Colors(color.Bgr24).green);
+    try helpers.expectEq(bgr24_pixels.bgr24[2], Colors(color.Bgr24).blue);
+    try helpers.expectEq(bgr24_pixels.bgr24[3], Colors(color.Bgr24).white);
+}
+
+test "PixelFormatConverter: convert from indexed2 to bgra32" {
+    const indexed2_pixels = try color.PixelStorage.init(helpers.zigimg_test_allocator, .indexed2, 4);
+    defer indexed2_pixels.deinit(helpers.zigimg_test_allocator);
+
+    indexed2_pixels.indexed2.palette[0] = Colors(color.Rgba32).red;
+    indexed2_pixels.indexed2.palette[1] = Colors(color.Rgba32).green;
+    indexed2_pixels.indexed2.palette[2] = Colors(color.Rgba32).blue;
+    indexed2_pixels.indexed2.palette[3] = Colors(color.Rgba32).white;
+
+    indexed2_pixels.indexed2.indices[0] = 0;
+    indexed2_pixels.indexed2.indices[1] = 1;
+    indexed2_pixels.indexed2.indices[2] = 2;
+    indexed2_pixels.indexed2.indices[3] = 3;
+
+    const bgra32_pixels = try PixelFormatConverter.convert(helpers.zigimg_test_allocator, &indexed2_pixels, .bgra32);
+    defer bgra32_pixels.deinit(helpers.zigimg_test_allocator);
+
+    try helpers.expectEq(bgra32_pixels.bgra32[0], Colors(color.Bgra32).red);
+    try helpers.expectEq(bgra32_pixels.bgra32[1], Colors(color.Bgra32).green);
+    try helpers.expectEq(bgra32_pixels.bgra32[2], Colors(color.Bgra32).blue);
+    try helpers.expectEq(bgra32_pixels.bgra32[3], Colors(color.Bgra32).white);
+}
+
+test "PixelFormatConverter: convert from indexed2 to rgb48" {
+    const indexed2_pixels = try color.PixelStorage.init(helpers.zigimg_test_allocator, .indexed2, 4);
+    defer indexed2_pixels.deinit(helpers.zigimg_test_allocator);
+
+    indexed2_pixels.indexed2.palette[0] = Colors(color.Rgba32).red;
+    indexed2_pixels.indexed2.palette[1] = Colors(color.Rgba32).green;
+    indexed2_pixels.indexed2.palette[2] = Colors(color.Rgba32).blue;
+    indexed2_pixels.indexed2.palette[3] = Colors(color.Rgba32).white;
+
+    indexed2_pixels.indexed2.indices[0] = 0;
+    indexed2_pixels.indexed2.indices[1] = 1;
+    indexed2_pixels.indexed2.indices[2] = 2;
+    indexed2_pixels.indexed2.indices[3] = 3;
+
+    const rgb48_pixelss = try PixelFormatConverter.convert(helpers.zigimg_test_allocator, &indexed2_pixels, .rgb48);
+    defer rgb48_pixelss.deinit(helpers.zigimg_test_allocator);
+
+    try helpers.expectEq(rgb48_pixelss.rgb48[0], Colors(color.Rgb48).red);
+    try helpers.expectEq(rgb48_pixelss.rgb48[1], Colors(color.Rgb48).green);
+    try helpers.expectEq(rgb48_pixelss.rgb48[2], Colors(color.Rgb48).blue);
+    try helpers.expectEq(rgb48_pixelss.rgb48[3], Colors(color.Rgb48).white);
+}
+
+test "PixelFormatConverter: convert from indexed2 to rgba64" {
+    const indexed2_pixels = try color.PixelStorage.init(helpers.zigimg_test_allocator, .indexed2, 4);
+    defer indexed2_pixels.deinit(helpers.zigimg_test_allocator);
+
+    indexed2_pixels.indexed2.palette[0] = Colors(color.Rgba32).red;
+    indexed2_pixels.indexed2.palette[1] = Colors(color.Rgba32).green;
+    indexed2_pixels.indexed2.palette[2] = Colors(color.Rgba32).blue;
+    indexed2_pixels.indexed2.palette[3] = Colors(color.Rgba32).white;
+
+    indexed2_pixels.indexed2.indices[0] = 0;
+    indexed2_pixels.indexed2.indices[1] = 1;
+    indexed2_pixels.indexed2.indices[2] = 2;
+    indexed2_pixels.indexed2.indices[3] = 3;
+
+    const rgba64_pixels = try PixelFormatConverter.convert(helpers.zigimg_test_allocator, &indexed2_pixels, .rgba64);
+    defer rgba64_pixels.deinit(helpers.zigimg_test_allocator);
+
+    try helpers.expectEq(rgba64_pixels.rgba64[0], Colors(color.Rgba64).red);
+    try helpers.expectEq(rgba64_pixels.rgba64[1], Colors(color.Rgba64).green);
+    try helpers.expectEq(rgba64_pixels.rgba64[2], Colors(color.Rgba64).blue);
+    try helpers.expectEq(rgba64_pixels.rgba64[3], Colors(color.Rgba64).white);
+}
+
+test "PixelFormatConverter: convert from indexed2 to Colorf32" {
+    const indexed2_pixels = try color.PixelStorage.init(helpers.zigimg_test_allocator, .indexed2, 4);
+    defer indexed2_pixels.deinit(helpers.zigimg_test_allocator);
+
+    indexed2_pixels.indexed2.palette[0] = Colors(color.Rgba32).red;
+    indexed2_pixels.indexed2.palette[1] = Colors(color.Rgba32).green;
+    indexed2_pixels.indexed2.palette[2] = Colors(color.Rgba32).blue;
+    indexed2_pixels.indexed2.palette[3] = Colors(color.Rgba32).white;
+
+    indexed2_pixels.indexed2.indices[0] = 0;
+    indexed2_pixels.indexed2.indices[1] = 1;
+    indexed2_pixels.indexed2.indices[2] = 2;
+    indexed2_pixels.indexed2.indices[3] = 3;
+
+    const float32_pixels = try PixelFormatConverter.convert(helpers.zigimg_test_allocator, &indexed2_pixels, .float32);
     defer float32_pixels.deinit(helpers.zigimg_test_allocator);
 
-    try helpers.expectEq(float32_pixels.float32[0], red_float32);
-    try helpers.expectEq(float32_pixels.float32[1], green_float32);
-    try helpers.expectEq(float32_pixels.float32[2], green_float32);
-    try helpers.expectEq(float32_pixels.float32[3], red_float32);
+    try helpers.expectEq(float32_pixels.float32[0], Colorsf32.red);
+    try helpers.expectEq(float32_pixels.float32[1], Colorsf32.green);
+    try helpers.expectEq(float32_pixels.float32[2], Colorsf32.blue);
+    try helpers.expectEq(float32_pixels.float32[3], Colorsf32.white);
 }
