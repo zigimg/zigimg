647ac012b3bac101023d0b3db6aee5176016912c
--global <saleemkhatib22@gmail.com>
2024-10-24 23:07:28 +0300

    refactor code and add write test for farbfeld

--- tests/formats/farbfeld_test.zig
+++ tests/formats/farbfeld_test.zig
@@ -2,50 +2,62 @@ const std = @import("std");
 const farbfeld = @import("../../src/formats/farbfeld.zig");
 const Image = @import("../../src/Image.zig");
 const ImageUnmanaged = @import("../../src/ImageUnmanaged.zig");
-const PixelFormat = @import("../../src/pixel_format.zig").PixelFormat;
 const helpers = @import("../helpers.zig");
 
 const testing = std.testing;
 const assert = std.debug.assert;
 
 test "check dimension file" {
-    const allocator = testing.allocator;
-    const yellow_file_path = "tests/test-suite/fixtures/" ++ "farbfeld/yellow-1x1-semitransparent.png.ff";
-    var yellow_image = try getff(allocator, yellow_file_path);
-    defer yellow_image.deinit(allocator);
+    const yellow_file_path = "yellow-1x1-semitransparent.png.ff";
+    var yellow_image = try getff(helpers.zigimg_test_allocator, yellow_file_path);
+    defer yellow_image.deinit(helpers.zigimg_test_allocator);
+
     try testing.expectEqual(yellow_image.width, 1);
     try testing.expectEqual(yellow_image.height, 1);
+    try testing.expect(yellow_image.pixels == .rgba64);
 
-    const dragon_path = "tests/test-suite/fixtures/" ++ "farbfeld/dragon.ff";
-    var dragon_image = try getff(allocator, dragon_path);
-    defer dragon_image.deinit(allocator);
+    const dragon_path = "dragon.ff";
+    var dragon_image = try getff(helpers.zigimg_test_allocator, dragon_path);
+    defer dragon_image.deinit(helpers.zigimg_test_allocator);
+
     try testing.expectEqual(dragon_image.width, 1680);
     try testing.expectEqual(dragon_image.height, 1167);
-
-    // try helpers.expectEq(try farbfeld.Farbfeld.formatDetect(&stream_source));
-
+    try testing.expect(dragon_image.pixels == .rgba64);
 }
 
 test "invalid file format" {
-    const allocator = testing.allocator;
-    const png_file_path = "tests/test-suite/fixtures/" ++ "farbfeld/dragon.png";
-    const image = getff(allocator, png_file_path);
+    const png_file_path = "dragon.png";
+    const image = getff(helpers.zigimg_test_allocator, png_file_path);
 
     try testing.expectError(farbfeld.ReadError.InvalidData, image);
 }
 
-fn getff(allocator: std.mem.Allocator, file_path: []const u8) !ImageUnmanaged {
-    const file = try std.fs.cwd().openFile(file_path, .{});
-    {
-        // var reader = file.reader();
-        // // for (0..8) |_| {
-        // //     std.debug.print("{c} ", .{try reader.readByte()});
-        // // }
-        // std.debug.print("magic match {}, file {s}\n", .{ try reader.isBytes(&farbfeld.Header.magic_value), file_path });
-    }
+test "read writeImage output" {
+    const dragon_path = "yellow-1x1-semitransparent.png.ff";
+    var source_image = try getff(helpers.zigimg_test_allocator, dragon_path);
+    defer source_image.deinit(helpers.zigimg_test_allocator);
+    var buf: [farbfeld.Header.size + @sizeOf(farbfeld.color.Rgba64) * 1]u8 = undefined;
+    var stream = Image.Stream{
+        .buffer = std.io.fixedBufferStream(&buf),
+    };
+
+    try farbfeld.Farbfeld.writeImage(helpers.zigimg_test_allocator, &stream, source_image, .{ .farbfeld = {} });
+    stream.buffer = std.io.fixedBufferStream(stream.buffer.getWritten());
+
+    var decoded_image = try farbfeld.Farbfeld.readImage(helpers.zigimg_test_allocator, &stream);
+    defer decoded_image.deinit(helpers.zigimg_test_allocator);
+
+    try helpers.expectEq(decoded_image.width, source_image.width);
+    try helpers.expectEq(decoded_image.height, source_image.height);
+    try testing.expect(decoded_image.pixels == .rgba64);
+}
+
+fn getff(allocator: std.mem.Allocator, comptime file_path: []const u8) !ImageUnmanaged {
+    const file = try helpers.testOpenFile(helpers.fixtures_path ++ "farbfeld/" ++ file_path);
     defer file.close();
+
     var stream_source = std.io.StreamSource{ .file = file };
 
-    const image_farb = try farbfeld.readImage(allocator, &stream_source);
+    const image_farb = try farbfeld.Farbfeld.readImage(allocator, &stream_source);
     return image_farb;
 }
