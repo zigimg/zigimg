9988a031baed1edaeeda05063ab556d73791e559
mlarouche <michael.larouche@coldbytesgames.com>
2025-02-06 09:25:27 -0500

    Remove the format directly from the zigimg root module

--- zigimg.zig
+++ zigimg.zig
@@ -1,24 +1,13 @@
-pub const bmp = @import("src/formats/bmp.zig");
 pub const color = @import("src/color.zig");
 pub const Colors = @import("src/predefined_colors.zig").Colors;
-pub const farbfeld = @import("src/formats/farbfeld.zig");
 pub const FormatInterface = @import("src/FormatInterface.zig");
 pub const formats = @import("src/formats.zig");
-pub const gif = @import("src/formats/gif.zig");
-pub const ilbm = @import("src/formats/ilbm.zig");
 pub const Image = @import("src/Image.zig");
 pub const ImageUnmanaged = @import("src/ImageUnmanaged.zig");
-pub const jpeg = @import("src/formats/jpeg.zig");
 pub const math = @import("src/math.zig");
-pub const netpbm = @import("src/formats/netpbm.zig");
 pub const OctTreeQuantizer = @import("src/OctTreeQuantizer.zig");
-pub const pam = @import("src/formats/pam.zig");
-pub const pcx = @import("src/formats/pcx.zig");
 pub const PixelFormat = @import("src/pixel_format.zig").PixelFormat;
 pub const PixelFormatConverter = @import("src/PixelFormatConverter.zig");
-pub const png = @import("src/formats/png.zig");
-pub const qoi = @import("src/formats/qoi.zig");
-pub const tga = @import("src/formats/tga.zig");
 
 test {
     const std = @import("std");
