9fd9ec1f47fc3458548d0b0b4e6f46f9ce30406a
mlarouche <michael.larouche@coldbytesgames.com>
2025-08-28 09:03:29 -0400

    Get io.ReadStream to a point where it supports everthing it needs

--- src/io.zig
+++ src/io.zig
@@ -1,14 +1,16 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 pub const ReadStream = union(enum) {
     memory: std.Io.Reader,
-    file: FileStream,
+    file: std.fs.File.Reader,
+    buffered_file: BufferedFileStream,
 
     pub const Error = SeekError || EndPosError;
-    pub const SeekError = std.fs.File.FileReader.SeekError;
-    pub const EndPosError = std.fs.File.FileReader.SizeError;
+    pub const SeekError = std.fs.File.Reader.SeekError;
+    pub const EndPosError = std.fs.File.Reader.SizeError;
 
-    const FileStream = struct {
+    const BufferedFileStream = struct {
         buffer: [4096]u8 = undefined,
         reader: std.fs.File.Reader = undefined,
     };
@@ -19,30 +21,60 @@ pub const ReadStream = union(enum) {
         };
     }
 
-    pub fn initFile(file: std.fs.File) ReadStream {
-        var file_stream: FileStream = .{};
+    pub fn initFile(file: std.fs.File, buffer: []u8) ReadStream {
+        return .{
+            .file = file.reader(buffer),
+        };
+    }
+
+    pub fn initBufferedFile(file: std.fs.File) ReadStream {
+        var file_stream: BufferedFileStream = .{};
 
         file_stream.reader = file.reader(file_stream.buffer[0..]);
 
         return .{
-            .file = file_stream,
+            .buffered_file = file_stream,
         };
     }
 
     pub fn reader(self: *ReadStream) *std.io.Reader {
         return switch (self.*) {
             .memory => |*memory_reader| memory_reader,
-            .file => |*file| &file.reader.interface,
+            .file => |*file_reader| &file_reader.interface,
+            .buffered_file => |*buffered_file| &buffered_file.reader.interface,
         };
     }
 
     pub fn seekTo(self: *ReadStream, offset: u64) SeekError!void {
         switch (self.*) {
             .memory => |*memory| {
+                if (offset >= memory.end) {
+                    return SeekError.Unseekable;
+                }
+
                 memory.seek = offset;
             },
-            .file => |*file| {
-                try file.reader.seekTo(offset);
+            .file => |*file_reader| {
+                const file_size = file_reader.getSize() catch {
+                    return SeekError.Unseekable;
+                };
+
+                if (offset >= file_size) {
+                    return SeekError.Unseekable;
+                }
+
+                try file_reader.seekTo(offset);
+            },
+            .buffered_file => |*buffered_file| {
+                const file_size = buffered_file.reader.getSize() catch {
+                    return SeekError.Unseekable;
+                };
+
+                if (offset >= file_size) {
+                    return SeekError.Unseekable;
+                }
+
+                try buffered_file.reader.seekTo(offset);
             },
         }
     }
@@ -50,10 +82,56 @@ pub const ReadStream = union(enum) {
     pub fn seekBy(self: *ReadStream, offset: i64) SeekError!void {
         switch (self.*) {
             .memory => |*memory| {
-                memory.seek += offset;
+                const new_pos: i64 = @as(i64, @intCast(memory.seek)) + offset;
+                if (new_pos < 0 or new_pos >= memory.end) {
+                    return std.fs.File.SeekError.Unseekable;
+                }
+
+                memory.seek = @intCast(new_pos);
             },
-            .file => |*file| {
-                try file.reader.seekBy(offset);
+            .file => |*file_reader| {
+                // Workaround seekBy not working properly (https://github.com/ziglang/zig/issues/25020)
+                var new_pos: i64 = @intCast(@as(i64, @intCast(file_reader.interface.seek)) + offset);
+                if (new_pos >= 0 and new_pos < file_reader.interface.end) {
+                    file_reader.interface.seek = @intCast(new_pos);
+                } else {
+                    file_reader.interface.seek = 0;
+                    file_reader.interface.end = 0;
+
+                    new_pos = @as(i64, @intCast(file_reader.pos)) + offset;
+
+                    const file_size = file_reader.getSize() catch {
+                        return std.fs.File.SeekError.Unseekable;
+                    };
+
+                    if (new_pos < 0 or new_pos >= file_size) {
+                        return std.fs.File.SeekError.Unseekable;
+                    }
+
+                    file_reader.pos = @intCast(new_pos);
+                }
+            },
+            .buffered_file => |*buffered_file| {
+                // Workaround seekBy not working properly (https://github.com/ziglang/zig/issues/25020)
+                var new_pos: i64 = @intCast(@as(i64, @intCast(buffered_file.reader.interface.seek)) + offset);
+                if (new_pos >= 0 and new_pos < buffered_file.reader.interface.end) {
+                    buffered_file.reader.interface.seek = @intCast(new_pos);
+                } else {
+                    buffered_file.reader.interface.seek = 0;
+                    buffered_file.reader.interface.end = 0;
+
+                    new_pos = @as(i64, @intCast(buffered_file.reader.pos)) + offset;
+
+                    const file_size = buffered_file.reader.getSize() catch {
+                        return std.fs.File.SeekError.Unseekable;
+                    };
+
+                    if (new_pos < 0 or new_pos >= file_size) {
+                        return std.fs.File.SeekError.Unseekable;
+                    }
+
+                    buffered_file.reader.pos = @intCast(new_pos);
+                }
             },
         }
     }
@@ -61,14 +139,16 @@ pub const ReadStream = union(enum) {
     pub fn getPos(self: *const ReadStream) u64 {
         return switch (self.*) {
             .memory => |*memory| memory.seek,
-            .file => |*file| file.reader.logicalPos(),
+            .file => |*file_reader| file_reader.logicalPos(),
+            .buffered_file => |*buffered_file| buffered_file.reader.logicalPos(),
         };
     }
 
     pub fn getEndPos(self: *ReadStream) EndPosError!u64 {
         return switch (self.*) {
             .memory => |*memory| memory.buffer.len,
-            .file => |*file| try file.reader.getSize(),
+            .file => |*file_reader| file_reader.getSize(),
+            .buffered_file => |*buffered_file| buffered_file.reader.getSize(),
         };
     }
 };
--- tests/io_test.zig
+++ tests/io_test.zig
@@ -4,7 +4,51 @@ const zigimg = @import("zigimg");
 
 const TEST_FILE_CONTENTS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-test "io.ReadStream: should read and seek properly within a file" {
+test "io.ReadStream: should read and seek properly within memory" {
+    var read_stream = zigimg.io.ReadStream.initMemory(TEST_FILE_CONTENTS[0..]);
+
+    var reader = read_stream.reader();
+
+    const read_inside_buffer = try reader.take(3);
+    try helpers.expectEq(read_stream.getPos(), 3);
+    try helpers.expectEqSlice(u8, read_inside_buffer[0..3], TEST_FILE_CONTENTS[0..3]);
+
+    try read_stream.seekTo(0);
+
+    const read_beginning_again = try reader.take(3);
+    try helpers.expectEqSlice(u8, read_beginning_again, read_inside_buffer);
+
+    try read_stream.seekBy(-3);
+    try helpers.expectEq(read_stream.getPos(), 0);
+
+    try read_stream.seekBy(5);
+    try helpers.expectEq(read_stream.getPos(), 5);
+
+    try read_stream.seekBy(1);
+    try helpers.expectEq(read_stream.getPos(), 6);
+
+    const end_pos = try read_stream.getEndPos();
+    try helpers.expectEq(end_pos, 36);
+
+    const read_remaining = try reader.take(30);
+    try helpers.expectEq(read_stream.getPos(), end_pos);
+    try helpers.expectEqSlice(u8, read_remaining, TEST_FILE_CONTENTS[6..]);
+}
+
+test "io.ReadStream: should error on invalid seek within memory" {
+    var read_stream = zigimg.io.ReadStream.initMemory(TEST_FILE_CONTENTS[0..]);
+
+    const seek_to_error = read_stream.seekTo(123456);
+    try helpers.expectError(seek_to_error, zigimg.io.ReadStream.SeekError.Unseekable);
+
+    const seek_by_after_end_error = read_stream.seekBy(123456);
+    try helpers.expectError(seek_by_after_end_error, zigimg.io.ReadStream.SeekError.Unseekable);
+
+    const seek_by_before_error = read_stream.seekBy(-300);
+    try helpers.expectError(seek_by_before_error, zigimg.io.ReadStream.SeekError.Unseekable);
+}
+
+test "io.ReadStream: should read and seek properly within a unbuffered file" {
     const TEST_FILENAME = "io_read_stream_test.dat";
 
     var temp_folder = std.testing.tmpDir(.{});
@@ -15,11 +59,167 @@ test "io.ReadStream: should read and seek properly within a file" {
     var read_file = try temp_folder.dir.openFile(TEST_FILENAME, .{});
     defer read_file.close();
 
-    var read_stream = zigimg.io.ReadStream.initFile(read_file);
+    var read_stream = zigimg.io.ReadStream.initFile(read_file, &.{});
+
+    var reader = read_stream.reader();
+
+    var read_inside_buffer: [3]u8 = @splat(0);
+    try reader.readSliceAll(read_inside_buffer[0..]);
+    try helpers.expectEq(read_stream.getPos(), 3);
+    try helpers.expectEqSlice(u8, read_inside_buffer[0..3], TEST_FILE_CONTENTS[0..3]);
+
+    try read_stream.seekTo(0);
+
+    var read_beginning_again: [3]u8 = @splat(0);
+    try reader.readSliceAll(read_beginning_again[0..]);
+    try helpers.expectEqSlice(u8, read_beginning_again[0..], read_inside_buffer[0..]);
+
+    try read_stream.seekBy(-3);
+    try helpers.expectEq(read_stream.getPos(), 0);
+
+    try read_stream.seekBy(5);
+    try helpers.expectEq(read_stream.getPos(), 5);
+
+    try read_stream.seekBy(1);
+    try helpers.expectEq(read_stream.getPos(), 6);
+
+    const end_pos = try read_stream.getEndPos();
+    try helpers.expectEq(end_pos, 36);
+
+    var read_remaining: [30]u8 = @splat(0);
+    try reader.readSliceAll(read_remaining[0..]);
+    try helpers.expectEq(read_stream.getPos(), end_pos);
+    try helpers.expectEqSlice(u8, read_remaining[0..], TEST_FILE_CONTENTS[6..]);
+}
+
+test "io.ReadStream: should error on invalid seek within unbuffered file" {
+    const TEST_FILENAME = "io_read_stream_test.dat";
+
+    var temp_folder = std.testing.tmpDir(.{});
+    defer temp_folder.cleanup();
+
+    try temp_folder.dir.writeFile(.{ .sub_path = TEST_FILENAME, .data = TEST_FILE_CONTENTS });
+
+    var read_file = try temp_folder.dir.openFile(TEST_FILENAME, .{});
+    defer read_file.close();
+
+    var read_stream = zigimg.io.ReadStream.initFile(read_file, &.{});
+
+    const seek_to_error = read_stream.seekTo(123456);
+    try helpers.expectError(seek_to_error, zigimg.io.ReadStream.SeekError.Unseekable);
+
+    const seek_by_after_end_error = read_stream.seekBy(123456);
+    try helpers.expectError(seek_by_after_end_error, zigimg.io.ReadStream.SeekError.Unseekable);
+
+    const seek_by_before_error = read_stream.seekBy(-300);
+    try helpers.expectError(seek_by_before_error, zigimg.io.ReadStream.SeekError.Unseekable);
+}
+
+test "io.ReadStream: should read and seek properly within a file with a small buffer" {
+    const TEST_FILENAME = "io_read_stream_test.dat";
+
+    var temp_folder = std.testing.tmpDir(.{});
+    defer temp_folder.cleanup();
+
+    try temp_folder.dir.writeFile(.{ .sub_path = TEST_FILENAME, .data = TEST_FILE_CONTENTS });
+
+    var read_file = try temp_folder.dir.openFile(TEST_FILENAME, .{});
+    defer read_file.close();
+
+    var small_buffer: [30]u8 = @splat(0);
+    var read_stream = zigimg.io.ReadStream.initFile(read_file, small_buffer[0..]);
 
     var reader = read_stream.reader();
 
     const read_inside_buffer = try reader.take(3);
     try helpers.expectEq(read_stream.getPos(), 3);
     try helpers.expectEqSlice(u8, read_inside_buffer[0..3], TEST_FILE_CONTENTS[0..3]);
+
+    try read_stream.seekTo(0);
+
+    const read_beginning_again = try reader.take(3);
+    try helpers.expectEqSlice(u8, read_beginning_again, read_inside_buffer);
+
+    try read_stream.seekBy(-3);
+    try helpers.expectEq(read_stream.getPos(), 0);
+
+    try read_stream.seekBy(5);
+    try helpers.expectEq(read_stream.getPos(), 5);
+
+    try read_stream.seekBy(1);
+    try helpers.expectEq(read_stream.getPos(), 6);
+
+    const end_pos = try read_stream.getEndPos();
+    try helpers.expectEq(end_pos, 36);
+
+    const read_remaining = try reader.take(30);
+    try helpers.expectEq(read_stream.getPos(), end_pos);
+    try helpers.expectEqSlice(u8, read_remaining, TEST_FILE_CONTENTS[6..]);
+}
+
+test "io.ReadStream: should read and seek properly within a buffered file" {
+    const TEST_FILENAME = "io_read_stream_test.dat";
+
+    var temp_folder = std.testing.tmpDir(.{});
+    defer temp_folder.cleanup();
+
+    try temp_folder.dir.writeFile(.{ .sub_path = TEST_FILENAME, .data = TEST_FILE_CONTENTS });
+
+    var read_file = try temp_folder.dir.openFile(TEST_FILENAME, .{});
+    defer read_file.close();
+
+    var read_stream = zigimg.io.ReadStream.initBufferedFile(read_file);
+
+    var reader = read_stream.reader();
+
+    const read_inside_buffer = try reader.take(3);
+    try helpers.expectEq(read_stream.getPos(), 3);
+    try helpers.expectEqSlice(u8, read_inside_buffer[0..3], TEST_FILE_CONTENTS[0..3]);
+
+    try read_stream.seekTo(0);
+
+    const read_beginning_again = try reader.take(3);
+    try helpers.expectEqSlice(u8, read_beginning_again, read_inside_buffer);
+
+    try read_stream.seekBy(-3);
+    try helpers.expectEq(read_stream.getPos(), 0);
+
+    try read_stream.seekBy(5);
+    try helpers.expectEq(read_stream.getPos(), 5);
+
+    try read_stream.seekBy(1);
+    try helpers.expectEq(read_stream.getPos(), 6);
+
+    const end_pos = try read_stream.getEndPos();
+    try helpers.expectEq(end_pos, 36);
+
+    const read_remaining = try reader.take(30);
+    try helpers.expectEq(read_stream.getPos(), end_pos);
+    try helpers.expectEqSlice(u8, read_remaining, TEST_FILE_CONTENTS[6..]);
+}
+
+test "io.ReadStream: should error on invalid seek within a default buffered file" {
+    const TEST_FILENAME = "io_read_stream_test.dat";
+
+    var temp_folder = std.testing.tmpDir(.{});
+    defer temp_folder.cleanup();
+
+    try temp_folder.dir.writeFile(.{ .sub_path = TEST_FILENAME, .data = TEST_FILE_CONTENTS });
+
+    var read_file = try temp_folder.dir.openFile(TEST_FILENAME, .{});
+    defer read_file.close();
+
+    var read_stream = zigimg.io.ReadStream.initBufferedFile(read_file);
+
+    const seek_to_error = read_stream.seekTo(123456);
+    try helpers.expectError(seek_to_error, zigimg.io.ReadStream.SeekError.Unseekable);
+
+    const seek_by_after_end_error = read_stream.seekBy(123456);
+    try helpers.expectError(seek_by_after_end_error, zigimg.io.ReadStream.SeekError.Unseekable);
+
+    const seek_by_before_error = read_stream.seekBy(-300);
+    try helpers.expectError(seek_by_before_error, zigimg.io.ReadStream.SeekError.Unseekable);
+
+    const seek_by_after_within_buffer_error = read_stream.seekBy(TEST_FILE_CONTENTS.len + 5);
+    try helpers.expectError(seek_by_after_within_buffer_error, zigimg.io.ReadStream.SeekError.Unseekable);
 }
