03d0ab5452d698b8013c505e2d138843b9ea0da4
Jesse Miller <jmiller@jmiller.com>
2026-09-22 16:53:57 -0600

    Allow a special-purpose block between a GIF graphic control and its image
    Some encoders write the Netscape loop extension after the first graphic-control extension and before the image descriptor. The reader treated that as the frame body and returned InvalidData.

--- tests/formats/gif_test.zig
+++ tests/formats/gif_test.zig
@@ -612,6 +612,23 @@ test "GIF test suite" {
     }
 }
 
+test "GIF Netscape extension between graphic control and image" {
+    var read_buffer: [zigimg.io.DEFAULT_BUFFER_SIZE]u8 = undefined;
+    var img = try helpers.testImageFromFileWithAllocator(
+        helpers.zigimg_test_allocator,
+        test_io,
+        helpers.fixtures_path ++ "gif/gce-then-netscape.gif",
+        read_buffer[0..],
+    );
+    defer img.deinit(helpers.zigimg_test_allocator);
+
+    try helpers.expectEq(img.width, 2);
+    try helpers.expectEq(img.height, 2);
+    try helpers.expectEq(img.animation.frames.items.len, 1);
+    try helpers.expectEq(img.animation.loop_count, zigimg.Image.AnimationLoopInfinite);
+    try helpers.expectApproxEqAbs(img.animation.frames.items[0].duration, 0.05, 0.001);
+}
+
 test "Rotating Earth GIF" {
     const gif_input_file = try helpers.testOpenFile(test_io, helpers.fixtures_path ++ "gif/rotating_earth.gif");
     defer gif_input_file.close(test_io);
